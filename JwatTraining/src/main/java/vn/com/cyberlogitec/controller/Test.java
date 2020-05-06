f359ac03e76a94a27a607cde2e0e3f8a9f8368ce
AnPhan0910 <ptan1999bdvn@gmail.com>
2020-05-06 15:04:11 +0700

    Update test.java

--- JwatTraining/src/main/java/vn/com/cyberlogitec/controller/Test.java
+++ JwatTraining/src/main/java/vn/com/cyberlogitec/controller/Test.java
@@ -3,5 +3,6 @@ package vn.com.cyberlogitec.controller;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Nothing to show!");
+		System.out.print("Hello");
 	}
 }
